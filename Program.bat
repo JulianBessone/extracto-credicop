c21ca40041c43a785b45d2a09b1c175435204814
JulianBessone <juliangaspar1997@gmail.com>
2024-04-22 08:45:12 -0300

    copy newfile

--- Program.bat
+++ Program.bat
@@ -1,10 +1,4 @@
 @echo off
-REM Define el directorio en el que deseas trabajar
-set "directorio=dev\ext"
-
-REM Navega al directorio deseado
-cd /d "%directorio%"
-cls
 
 echo.
 echo.
@@ -23,6 +17,30 @@ echo.                          Make with 🧠 - 2023
 echo.
 echo.
 echo.
+
+REM Define el directorio en el que deseas trabajar
+set "directorio=C:\dev\ext"
+
+set "descargas=C:\Users\JGBessone\Downloads"
+
+REM Navega al directorio deseado
+cd /d "%descargas%"
+
+REM Encuentra el archivo más reciente con extensión .xlsx en la carpeta de origen
+for /f "delims=" %%I in ('forfiles /p "%descargas%" /m *.xlsx /c "cmd /c echo @file @fdate" /d -1 2^>nul') do set "latestFile=%%I"
+
+REM Verifica si se encontró un archivo
+if defined latestFile (
+    REM Copia el archivo encontrado a la carpeta de destino
+    copy "%descargas%\%latestFile%" "%directorio%"
+    echo Archivo copiado exitosamente.
+) else (
+    echo No se encontraron archivos .xlsx en la carpeta de origen.
+)
+
+REM Navega al directorio deseado
+cd /d "%directorio%"
+cls
 
 REM Pregunta al usuario si desea instalar las dependencias
 set /p "respuesta=¿Deseas instalar las dependencias? (S/N): "
